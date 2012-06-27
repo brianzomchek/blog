6554dbf02e441c49a936f1fffa97f5dd112e901e
brianzomchek <brian@varial>
2012-06-27 05:59:06 -0400

    first commit of display of blog posts

---
+++ app.js
@@ -0,0 +1,40 @@
+//Module Dependancies
+var express = require('express');
+
+var ArticleProvider = require('./articleprovider-memory').ArticleProvider;
+
+var app = express.createServer();
+
+//Configuration
+app.configure( function() {
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'jade');
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(require('stylus').middleware({ src: __dirname + '/public' }));
+	app.use(app.router);
+	app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+	app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+	});
+
+app.configure('producution', function(){
+	app.use(express.errorHandler());
+});
+
+var articleProvider = new ArticleProvider();
+
+app.get('/', function(req, res){
+	articleProvider.findAll( function(error,docs) {
+		res.render('index.jade', {
+			title: 'Blog',
+			articles:docs
+			}
+		);
+	})
+});
+
+
+app.listen(3000);
